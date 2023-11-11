ded7d9c53e935b6a95796f13fa9f38e1ce1e2d3a
WINDARK-coder <telmanbleach1998@gmail.com>
2023-11-11 18:16:31 +0400

    logo

--- resources/views/admin/inc/left_sidebar.blade.php
+++ resources/views/admin/inc/left_sidebar.blade.php
@@ -2,7 +2,7 @@
      <!-- LOGO -->
      <div class="navbar-brand-box">
          <!-- Dark Logo-->
-         <a href="{{ route('home') }}" class="logo logo-dark">
+         {{-- <a href="{{ route('home') }}" class="logo logo-dark">
              <img src="{{ asset('admin/assets/images/logo-dark.png') }}" alt="" height="22">
              <span class="logo-sm">
                  <img src="{{ asset('admin/assets/images/logo-dark.png') }}" alt="" height="22">
@@ -10,17 +10,16 @@
              <span class="logo-lg">
                  <img src="{{ asset('admin/assets/images/asanexlogo1.png') }}" alt="" height="17">
              </span>
-         </a>
+         </a> --}}
          <!-- Light Logo-->
-         <a href="{{ route('home') }}" class="logo logo-light">
+         {{-- <a href="{{ route('home') }}" class="logo logo-light">
              <span class="logo-sm">
                  <img src="{{ asset('admin/assets/images/asanexlogo2.png') }}" alt="" height="22">
              </span>
              <span class="logo-lg">
-                 {{-- logo place --}}
                  <img src="{{ asset('admin/assets/images/asanexlogo1.png') }}" alt="" height="17">
              </span>
-         </a>
+         </a> --}}
          <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover"
              id="vertical-hover">
              <i class="ri-record-circle-line"></i>
