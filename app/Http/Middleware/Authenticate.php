300c319583d1a3905545dbc16fab79ad50a009a1
WINDARK-coder <telmanbleach1998@gmail.com>
2023-11-12 19:13:14 +0400

    contact middlware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,6 +14,7 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
+        \Log::info('Redirecting...');
         // Check if the request is for the API
         if ($request->is('api/*')) {
             return null; // Do not redirect for API requests
