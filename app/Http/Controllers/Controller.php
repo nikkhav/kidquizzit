464e3ac2fe3aa9f6f27ca999dbf1e29eee0e7403
WINDARK-coder <telmanbleach1998@gmail.com>
2023-11-12 19:51:33 +0400

    contact middlware

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -14,20 +14,6 @@ class Controller extends BaseController
 
     public function canOrAbort($permission, $defaultCode = 401)
     {
-        // Check if there is an authenticated user
-        $user = auth()->user();
-
-        // If the user is authenticated and has the necessary permission, allow access
-        if ($user && $user->can($permission)) {
-            return;
-        }
-
-        // If the user is not authenticated, continue without aborting
-        if (!$user) {
-            return;
-        }
-
-        // If the user is authenticated but does not have the necessary permission, abort with the specified status code
-        abort($defaultCode);
+        if (!auth()->user()->can($permission)) abort($defaultCode);
     }
 }
