7cdeaf4b91d625eb5fcbd3925dbffe0d1461511b
WINDARK-coder <telmanbleach1998@gmail.com>
2023-11-12 19:50:27 +0400

    contact middlware

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -12,14 +12,22 @@ class Controller extends BaseController
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
 
-    public function canOrAbort($permission, $message = null, $defaultCode = 401)
+    public function canOrAbort($permission, $defaultCode = 401)
     {
-        if (!auth()->user()->can($permission)) {
-            if ($message) {
-                abort($defaultCode, $message);
-            } else {
-                abort($defaultCode);
-            }
+        // Check if there is an authenticated user
+        $user = auth()->user();
+
+        // If the user is authenticated and has the necessary permission, allow access
+        if ($user && $user->can($permission)) {
+            return;
         }
+
+        // If the user is not authenticated, continue without aborting
+        if (!$user) {
+            return;
+        }
+
+        // If the user is authenticated but does not have the necessary permission, abort with the specified status code
+        abort($defaultCode);
     }
 }
