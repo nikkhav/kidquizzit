b182a6f98023d91e1cfbfc847ba8053dd38fa2b0
WINDARK-coder <telmanbleach1998@gmail.com>
2023-11-12 19:02:24 +0400

    contact middlware

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,7 @@ Route::middleware(['auth'])->group(function () {
     Route::resource('whyquestion', WhyQuestionController::class);
     Route::resource('quiz', QuizController::class);
     Route::resource('contact', ContactController::class)->except(['store']);
-    Route::post('contact', [ContactController::class, 'store'])->name('contact.store');
+    // Route::post('contact', [ContactController::class, 'store'])->name('contact.store');
 
     Route::put('contact/{status}', [ContactController::class, 'status'])->name('contact.status');
     Route::resource('quizquestion', QuizQuestionController::class)->except('show');
